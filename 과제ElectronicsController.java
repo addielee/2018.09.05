8e34531763ed3ddadd6adf8f1368fdfa6379719d
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-05 18:18:41 +0900

    Create 과제ElectronicsController.java

---
+++ 과제ElectronicsController.java
@@ -0,0 +1,52 @@
+/**
+	View로부터 사용자 요청을 받아서 그에 해당하는 Service를 호출하고
+	그 결과를 받아 결과에 해당하는 View를 호출한다.
+*/
+
+class ElectronicsController{
+      static ElectronicsService service = ElectronicsService.getService();   //모두가 다 사용할 거니깐 위로 뺌
+
+	/**
+		1.전체검색
+	*/
+
+	public static void selectAll(){  
+		//전체검색이라 따로 인수는 없음, 어차피 EndView에서 끝나기 때문에 StartView로 가긴 해도 결국 출력할 것은 없음(따라서 return 필요없음)
+
+		//service 호출 -> 그 결과 받아서 -> EndView 호출
+		Electronics[] elecArr = service.selectAll();
+		EndView.printAll(elecArr);
+	}
+
+	/**
+		2.조건검색
+	*/
+
+	public static void searchByModelNo(int modelNo){
+		//service 호출 -> 그 결과 받아서 -> EndView 호출
+		Electronics elec = service.searchByModelNo(modelNo);
+
+		if(elec == null) EndView.printMessage(modelNo+"는 없는 정보입니다.");
+		else EndView.printSearch(elec);
+		
+	}
+
+	/**
+		3.수정하기
+	*/
+
+	public static void update(Electronics elec){   //3개 다 오니깐
+		//service 호출 -> 그 결과 받아서 -> EndView 호출
+		Electronics elec = service.update(elec);
+
+		if(elec ==false) EndView.printMessage("수정 실패");
+		else EndView.printMessage("수정 성공");
+	}
+
+	/**
+		4.등록하기
+	*/
+
+	public static void insert(Electronics elec){  //등록할 것 4개니깐, 외부에서 접근 가능하도록 static 붙임
+	}
+}
